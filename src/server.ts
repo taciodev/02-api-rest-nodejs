52e8fdfa52b0907eff257eba6442c689603739c7
taciano <tacianohora2020@hotmail.com>
2023-04-03 14:07:52 -0300

    feat: configurar o knex

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify'
+import { knex } from './databse'
+const app = fastify()
+
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+  return tables
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server is running')
+  })
+  .catch(() => {
+    console.log('Error')
+  })
