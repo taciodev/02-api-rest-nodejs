e64c9808e3cab8f8df7530e85856808e9cc73dce
taciano <tacianohora2020@hotmail.com>
2023-04-10 11:54:08 -0300

    feat: criação de transação

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,9 +1,26 @@
 import { FastifyInstance } from 'fastify'
+import crypto from 'node:crypto'
+import { z as Schema } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transaction = await knex('transactions').select('*')
-    return transaction
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = Schema.object({
+      title: Schema.string(),
+      amount: Schema.number(),
+      type: Schema.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
